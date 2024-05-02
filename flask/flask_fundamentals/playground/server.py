a89d50c6f258aff17e179348bc0e4fcffccc7209
Mustafa taha <mustafa.taha.mu95@gmail.com>
2024-05-02 15:29:07 +0300

    update playground

--- flask/flask_fundamentals/playground/server.py
+++ flask/flask_fundamentals/playground/server.py
@@ -2,8 +2,10 @@ from flask import Flask,render_template
 
 app = Flask(__name__)
 
-
-@app.route("/play")
+@app.route("/")
+def start():
+    return "<h1 style='margin: 50px auto;text-align:center;'>Welecome, enter localhost:5000/play/num/color</h1>"
+@app.route("/play/")
 def play():
     return render_template("index.html",x=3,color="#9fc5f8")
 
