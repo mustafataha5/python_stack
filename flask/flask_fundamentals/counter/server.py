fd8460c9d75e3beafadfa897d3d3ac4bc7fd9304
Mustafa taha <mustafa.taha.mu95@gmail.com>
2024-05-07 16:50:25 +0300

    Add counter

---
+++ flask/flask_fundamentals/counter/server.py
@@ -0,0 +1,36 @@
+from flask import Flask,render_template,redirect,request,session
+
+
+
+app = Flask(__name__)
+app.secret_key = 'keep it secret, keep it safe'
+
+@app.route("/")
+def show():
+    if 'count' in session: 
+        session['count'] = session['count'] + 1
+    else: 
+        session['count'] = 1     
+    return render_template("index.html")
+
+@app.route("/increamt",methods=['POST'])
+def make_count():
+    #print(request.form)
+    # if 'count' in session: 
+    session['count'] = session['count'] + 1
+    # else: 
+    #     session['count'] = 1 
+    return redirect("/")     
+
+@app.route("/reset",methods=['POST'])
+def reset_count():
+    #session.clear()		# clears all keys
+    session.pop('count')		# clears a specific key
+    return redirect("/")
+
+
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
