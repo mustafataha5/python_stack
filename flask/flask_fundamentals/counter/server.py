d2acb251bb0c08528268ef2f0e297532b0b2f9e5
Mustafa taha <mustafa.taha.mu95@gmail.com>
2024-05-07 18:15:52 +0300

    Update counter task 6

--- flask/flask_fundamentals/counter/server.py
+++ flask/flask_fundamentals/counter/server.py
@@ -16,7 +16,7 @@ def show():
 @app.route("/increamt",methods=['POST'])
 def make_count():
     #print(request.form)
-    # if 'count' in session: 
+    # if 'count' in session:
     session['count'] = session['count'] + 1
     # else: 
     #     session['count'] = 1 
@@ -24,12 +24,23 @@ def make_count():
 
 @app.route("/destroy_session",methods=['POST'])
 def reset_count():
-    #session.clear()		# clears all keys
-    session.pop('count')		# clears a specific key
+    session.clear()		# clears all keys
+    #session.pop('count')		# clears a specific key
     return redirect("/")
 
 
+@app.route("/increamt_by_n",methods=['POST'])
+def makei_count():
+    #print(request.form)
+    # if 'count' in session:
+    if request.form['n'].isnumeric():
+        session['n'] = int(request.form['n']) 
+        if session['n'] >= 1 :
+            session['count'] = session['count'] + session['n']-1 
 
+    # else: 
+    #     session['count'] = 1 
+    return redirect("/")   
 
 
 if __name__=="__main__":
