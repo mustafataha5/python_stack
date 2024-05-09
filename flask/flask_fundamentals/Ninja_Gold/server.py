0e3176c6ebbb563aec285ebb1bb3b23e4b9793c5
Mustafa taha <mustafa.taha.mu95@gmail.com>
2024-05-09 23:46:55 +0300

    update Ninja Gold

--- flask/flask_fundamentals/Ninja_Gold/server.py
+++ flask/flask_fundamentals/Ninja_Gold/server.py
@@ -12,26 +12,26 @@ def index():
     if not 'total_gold' in session : 
         session['total_gold'] = 0 
         session['message'] = ""
+        session['counter']=0
+        session['win'] = False
+        session['game_finish'] = False
+        
     return render_template("index.html")
 
 
 
 @app.route("/process_money",methods=['POST'])
 def process():
-    print(request.form['building'])
+    
+    dict = {
+        'farm': random.randint(10,20),
+        'cave': random.randint(5,10),
+        'house':random.randint(2,5),
+        'casino': random.randint(-50,50)
+    }
+    print('*************',session)
     from_input = request.form['building']
-    gold = 0
-    if from_input == "farm": 
-        
-       gold = random.randint(10,20)
-    elif from_input == "cave": 
-        gold = random.randint(5,10)
-    elif from_input == "house":
-       gold = random.randint(2,5)
-    elif from_input == "casino": 
-        gold =random.randint(-50,50)
-    
-    
+    gold = dict[from_input]
     named_tuple = time.localtime() # get struct_time
     time_string = time.strftime("%Y/%m/%d, %H:%M:%S %p", named_tuple)    
     
@@ -39,7 +39,18 @@ def process():
         session['message'] = "<li class='green'>Earn "+str(gold)+" golds from "+from_input+"!( "+time_string+")</li>"+session['message']      
     else: 
         session['message'] = "<li class='red'>Enter casino and lost "+str(gold)+" golds ... Ouch!!!... ( "+time_string+")</li>"+session['message']    
-    session['total_gold'] =  session['total_gold'] + gold  
+    
+    session['total_gold'] =  session['total_gold'] + gold 
+    session['counter'] = session['counter']+1
+    
+    if session['total_gold'] >= 100 and session['counter'] <= 15: #500:
+        session['game_finish'] = True
+        session['win'] = True
+    elif  session['counter'] > 15:
+        session['game_finish'] = True
+        session['win'] = False    
+        
+    
     return redirect("/")
 
 @app.route("/reset")
