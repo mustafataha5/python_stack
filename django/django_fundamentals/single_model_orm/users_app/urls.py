d57527bcce908f758ca91da0cbfbf8983c1c86c0
Mustafa taha <mustafa.taha.mu95@gmail.com>
2024-05-14 18:43:41 +0300

    Update single ORM users templates

--- django/django_fundamentals/single_model_orm/users_app/urls.py
+++ django/django_fundamentals/single_model_orm/users_app/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('',views.index),
-    path('addUser',views.add_user)
+    path('addUser',views.add_user),
+     path('edit/<int:id>',views.edit),
+     path('editUser/<int:id>',views.editUser),
+      path('delete/<int:id>',views.delete)
 ]
--- django/django_fundamentals/single_model_orm/users_app/views.py
+++ django/django_fundamentals/single_model_orm/users_app/views.py
@@ -25,4 +25,34 @@ def add_user(request):
         
     return  redirect('/')    
 
+def delete(request,id): 
+    del_user = Users.objects.get(id=id) 
+    del_user.delete()
+    return  redirect('/')   
 
+def edit(request,id): 
+    edit_user = Users.objects.get(id=id)
+    data = {
+        "user":edit_user, 
+    }
+    return  render(request,'editUser.html',data)  
+
+def editUser (request,id) : 
+    if request.method == "POST": 
+        if request.POST['first_name'] != '' : 
+            fname = request.POST['first_name']
+        if  request.POST['last_name'] != '' :    
+            lname = request.POST['last_name']
+        if  request.POST['email'] != '' :       
+            email = request.POST['email']
+        if  request.POST['age'] != '' and request.POST['age'].isnumeric(): 
+                 age = int(request.POST['age'])
+         
+        edit_user = Users.objects.get(id=id)
+        edit_user.first_name = fname 
+        edit_user.last_name = lname
+        edit_user.age = age
+        edit_user.email_address =email 
+        edit_user.save()         
+       
+    return redirect("/")     
